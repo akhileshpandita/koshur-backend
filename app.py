2393355487a0e20c926d75875d263ae599dce68c
akhileshpandita <akhileshpandita@berkeley.edu>
2025-03-10 00:20:02 -0700

    Fix app.run() placement

--- app.py
+++ app.py
@@ -124,5 +124,4 @@ def index():
 
 # ✅ **Move `app.run()` to this section**
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=port, debug=True)
-    pass  # Gunicorn will handle execution
+    gunicorn_app = app
